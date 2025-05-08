6fefc1c1f48d22f17ade08fa3f5aefe5b353976b
vwx12345 <vwx12345@gmail.com>
2025-05-08 20:17:36 +0900

    [3-1] spring boot project 환경구성

---
+++ src/main/java/com/example/fc_auth/FcAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.fc_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fc_auth/FcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fc_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
